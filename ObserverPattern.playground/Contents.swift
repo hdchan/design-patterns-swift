39f6182b9561b6e109162d4486fef5cd782e29a5
Henry Chan <henry.d.chan@gmail.com>
2020-02-29 08:01:04 -0500

    Update

--- ObserverPattern.playground/Contents.swift
+++ ObserverPattern.playground/Contents.swift
@@ -10,79 +10,44 @@ protocol Subject {
     func notify()
 }
 
-class ObserverComposite: Observer {
+class ConcreteSubject: Subject {
     
-    private var observers: [Observer]
+    struct State {}
     
-    init(observers: [Observer]) {
-        self.observers = observers
+    var state: State = State() {
+        didSet {
+            notify()
+        }
     }
     
-    func update() {
-        observers.forEach { $0.update() }
-    }
-}
-
-class WeatherStationHeadQuarter: Subject {
     private var observers: [Observer] = []
-    
-    public private(set) var temperature: Int?
     
     func attach(observer: Observer) {
         observers.append(observer)
     }
     
     func detach(observer: Observer) {
-        guard let index = observers.firstIndex(where: { $0 === observer }) else {
-            return
-        }
+        guard let index = observers.firstIndex(where: { $0 === observer }) else { return }
         observers.remove(at: index)
     }
     
     func notify() {
-        guard observers.count > 0 else {
-            print("No stations listening")
-            return
-        }
         observers.forEach { $0.update() }
-    }
-    
-    func setTemperature(temperature: Int) {
-        self.temperature = temperature
-        notify()
     }
 }
 
-class LocalWeatherStation: Observer {
-    private let stationNumber: Int
-    private let headQuarter: WeatherStationHeadQuarter
+class ConcreteObserver: Observer {
     
-    init(stationNumber: Int,  headQuarter: WeatherStationHeadQuarter) {
-        self.stationNumber = stationNumber
-        self.headQuarter = headQuarter
+    private weak var subject: ConcreteSubject?
+    
+    init(subject: ConcreteSubject) {
+        self.subject = subject
     }
     
     func update() {
-        guard let temperature = headQuarter.temperature else { return }
-        print("Weather station \(stationNumber) reports that it is: \(temperature) degrees")
+        guard let state = subject?.state else { return }
+        print(state)
     }
 }
 
-let hq = WeatherStationHeadQuarter()
-let ws1 = LocalWeatherStation(stationNumber: 1, headQuarter: hq)
-let ws2 = LocalWeatherStation(stationNumber: 2, headQuarter: hq)
-
-let allWS = ObserverComposite(observers: [ws1, ws2])
-
-hq.attach(observer: allWS)
-hq.setTemperature(temperature: 20)
-
-//hq.attach(observer: ws2)
-//hq.setTemperature(temperature: 50)
-//
-//hq.detach(observer: ws1)
-//hq.setTemperature(temperature: 60)
-//
-//hq.detach(observer: ws2)
-//hq.setTemperature(temperature: 99)
 
--- ObserverPatternExample.playground/Contents.swift
+++ ObserverPatternExample.playground/Contents.swift
@@ -10,44 +10,79 @@ protocol Subject {
     func notify()
 }
 
-class ConcreteSubject: Subject {
+class ObserverComposite: Observer {
     
-    struct State {}
+    private var observers: [Observer]
     
-    var state: State = State() {
-        didSet {
-            notify()
-        }
+    init(observers: [Observer]) {
+        self.observers = observers
     }
     
+    func update() {
+        observers.forEach { $0.update() }
+    }
+}
+
+class WeatherStationHeadQuarter: Subject {
     private var observers: [Observer] = []
+    
+    public private(set) var temperature: Int?
     
     func attach(observer: Observer) {
         observers.append(observer)
     }
     
     func detach(observer: Observer) {
-        guard let index = observers.firstIndex(where: { $0 === observer }) else { return }
+        guard let index = observers.firstIndex(where: { $0 === observer }) else {
+            return
+        }
         observers.remove(at: index)
     }
     
     func notify() {
+        guard observers.count > 0 else {
+            print("No stations listening")
+            return
+        }
         observers.forEach { $0.update() }
+    }
+    
+    func setTemperature(temperature: Int) {
+        self.temperature = temperature
+        notify()
     }
 }
 
-class ConcreteObserver: Observer {
+class LocalWeatherStation: Observer {
+    private let stationNumber: Int
+    private let headQuarter: WeatherStationHeadQuarter
     
-    private weak var subject: ConcreteSubject?
-    
-    init(subject: ConcreteSubject) {
-        self.subject = subject
+    init(stationNumber: Int,  headQuarter: WeatherStationHeadQuarter) {
+        self.stationNumber = stationNumber
+        self.headQuarter = headQuarter
     }
     
     func update() {
-        guard let state = subject?.state else { return }
-        print(state)
+        guard let temperature = headQuarter.temperature else { return }
+        print("Weather station \(stationNumber) reports that it is: \(temperature) degrees")
     }
 }
 
+let hq = WeatherStationHeadQuarter()
+let ws1 = LocalWeatherStation(stationNumber: 1, headQuarter: hq)
+let ws2 = LocalWeatherStation(stationNumber: 2, headQuarter: hq)
+
+let allWS = ObserverComposite(observers: [ws1, ws2])
+
+hq.attach(observer: allWS)
+hq.setTemperature(temperature: 20)
+
+//hq.attach(observer: ws2)
+//hq.setTemperature(temperature: 50)
+//
+//hq.detach(observer: ws1)
+//hq.setTemperature(temperature: 60)
+//
+//hq.detach(observer: ws2)
+//hq.setTemperature(temperature: 99)
 
